9f0f371ca31407a432c4a06cf37fd88f37362b45
Andrey <andreinew@mail.ru>
2018-08-22 14:09:42 +0000

    Added hoc - Layout, Auxiliary

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+import Auxiliary from '../Auxiliary/Auxiliary';
+import cssObject from './Layout.css';
+//import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+
+class Layout extends Component {
+    // state = { 
+    //     showSideDrawer: false 
+    // }
+    
+    // sideDrawerClosedHandler = () => {
+    //     this.setState({showSideDrawer: false});
+    // }
+
+    // sideDrawerToggleHandler = () => {
+    //     this.setState((prevState) => {
+    //         return  {showSideDrawer: !prevState.showSideDrawer };
+    //     });
+    // }
+
+    render () {
+        return(
+            <Auxiliary>
+                <main className={cssObject.Content}>
+                    {this.props.children}
+                </main>
+            </Auxiliary>
+        );
+    }
+}
+ 
+
+
+
+export default Layout;
