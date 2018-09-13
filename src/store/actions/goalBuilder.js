bf50651755e9e2ff909fa6b8a801a25b9f962bdc
Andrey <andreinew@mail.ru>
2018-09-13 11:08:15 +0000

    Working on goalBuilder.js action file

--- src/store/actions/goalBuilder.js
+++ src/store/actions/goalBuilder.js
@@ -1,2 +1,37 @@
 import * as actionTypes from './actionTypes';
 import axios from '../../axios-db';
+
+
+export const setupGoalSuccess = ( goalConfig ) => {
+    return {
+        type: actionTypes.SETUP_GOAL_SUCCESS,
+        goalConfig: goalConfig
+    };
+};
+
+// export const setupGoalFail = (error) => {
+//     return {
+//         type: actionTypes.SETUP_GOAL_FAIL,
+//         error: error
+//     };
+// };
+
+export const setupGoalStart = () => {
+    return {
+        type: actionTypes.SETUP_GOAL_START
+    };
+};
+
+export const setupGoal = ( goalConfig ) => {
+    return dispatch => {
+        dispatch(setupGoalStart());
+        axios.post('/goals', goalConfig)
+            .then( response => {
+                console.log(response.data.name);
+                dispatch(setupGoalSuccess(goalConfig));
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
+};
