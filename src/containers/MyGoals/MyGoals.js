ccff807472a98543d8bfd613a3e4f4ba1e15f107
AndreyKonovalenko <andreinew@mail.ru>
2018-09-16 22:05:05 +0300

    Copy fetchorder function from bb_project to MyGoals container

--- src/containers/MyGoals/MyGoals.js
+++ src/containers/MyGoals/MyGoals.js
@@ -9,6 +9,30 @@ class MyGoals extends Component {
         this.props.onFetchOrders(this.props.token, this.props.userId);
     }
    
+
+
+    xport const fetchOrders = (token, userId) => {
+        return dispatch  => {
+            dispatch(fetchOrdersStart());
+            const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
+            // orderBy - sintax provided by Firebase
+            axios.get('/orders.json' + queryParams)
+                .then(response => {
+                    const fetchOrders = [];
+                    for (let key in response.data){
+                        fetchOrders.push({
+                            ...response.data[key],
+                            id: key
+                        });
+                    }
+                    dispatch(fetchOrdersSuccess(fetchOrders));
+                })
+                .catch(error => {
+                    dispatch(fetchOrdersFail(error));
+                });
+        };
+    };
+
     
     render() {
         let goals = <GoalCard/>
