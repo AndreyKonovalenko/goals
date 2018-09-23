d6e39a1ae5cb7b293eaf8245e35f7e0a3d2c5cdc
AndreyKonovalenko <andreinew@mail.ru>
2018-09-23 19:35:43 +0300

    Small adjustments to GoalCard component

--- src/components/GoalCard/GoalCard.js
+++ src/components/GoalCard/GoalCard.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Button from '../UI/Button/Button';
 
 import cssObject from './GoalCard.css';
 
 const goalCard = (props) => {
 
     return (
-        <div className={cssObject.GoalCard}>
+        <div className={cssObject.GoalCard} onClick={props.redirectHandler}>
             <h3>{props.goalConfig.title.value}</h3>
+            <Button buttonType="Danger" clicked={props.delete}>Delete</Button>
         </div>
         
     );
