4612a2236a8a084f528ccf0fa5c6219e8b6d93cf
Andrey <andreinew@mail.ru>
2018-10-15 10:50:33 +0000

    Working on controsl

--- src/components/Navigation/NavItems/NavItems.js
+++ src/components/Navigation/NavItems/NavItems.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavItem from './NavItem/NavItem';
+import Button from '../../UI/Button/Button';
 import cssObject from './NavItems.css'
 
 import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
@@ -8,16 +9,22 @@ import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
 //should add dynemic adding path based on goal's tittle for each user
 const navItems = (props) => {
     const loginSigup = (
-            <Auxiliary>
-                <NavItem link="/auth/signup">Sign Up</NavItem>
-                <NavItem link="/auth/login">Log In</NavItem>
-            </Auxiliary>
-        );
+        <Auxiliary>
+            <NavItem link="/auth/signup">Sign Up</NavItem>
+            <NavItem link="/auth/login">Log In</NavItem>
+        </Auxiliary>
+    );
+    const controls =(
+        <Auxiliary>
+            <NavItem link="/builder">Add Goal</NavItem>
+            <Button>EDIT</Button>
+        </Auxiliary>
+    );
     return (
         <ul className={cssObject.NavItems}>
-        <NavItem link="/" exact>MyGoals</NavItem>
-        { props.isAuthenticated ? <NavItem link="/builder">Add Goal</NavItem>: null}
-        { !props.isAuthenticated ? loginSigup : <NavItem link="/logout">Logout</NavItem>}
+            { props.isAuthenticated ? controls: null}
+            <NavItem link="/" exact>MyGoals</NavItem>
+            { !props.isAuthenticated ? loginSigup : <NavItem link="/logout">Logout</NavItem>}
         </ul>
     );
 }
