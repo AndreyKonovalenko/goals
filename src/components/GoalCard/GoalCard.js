260c9a7263e6735d9be5376231c64884324fa911
Andrey <andreinew@mail.ru>
2018-10-16 14:38:08 +0000

    Changed the rule of displaying the delete button inside the  goalcard component

--- src/components/GoalCard/GoalCard.js
+++ src/components/GoalCard/GoalCard.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import Button from '../UI/Button/Button';
-import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
+//import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
 
 import cssObject from './GoalCard.css';
 
 const goalCard = (props) => {
 
     return (
-        <Auxiliary>
+        <div className={cssObject.GoalCardFlex}>
+            <Button buttonType="Danger" clicked={props.delete}>Delete</Button>
             <div className={cssObject.GoalCard} onClick={props.clicked}>
                 <h3>{props.title}</h3>
             </div>
-            <Button buttonType="Danger" clicked={props.delete}>Delete</Button>
-        </Auxiliary>
+        </div>
     );
     
 };
