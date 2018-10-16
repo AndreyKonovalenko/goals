aef7736837a52be92d05173126867898cdbd82ff
Andrey <andreinew@mail.ru>
2018-10-16 10:42:35 +0000

    Adede new component controls

---
+++ src/components/Navigation/Controls/Controls.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Button from '../../UI/Button/Button';
+import cssObject from './Controls.css';
+
+import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
+
+const controls = (props) => {
+    const controls =(
+        <Auxiliary>
+            <Button clicked={props.onAddGoal}>Add Goal</Button>
+            <Button >EDIT</Button>
+        </Auxiliary>
+    );
+    
+    return (
+        <ul className={cssObject.Controls}>
+            {props.isAuthenticated ? controls : null}
+        </ul>
+    );
+};
+
+export default controls;
+
