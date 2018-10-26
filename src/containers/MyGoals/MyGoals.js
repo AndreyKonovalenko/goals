f94b6ec8ab18e4695a4836ccc537f059ffb62598
Andrey <andreinew@mail.ru>
2018-10-26 10:31:57 +0000

    Moved animation logic to the hoc componet Animation

--- src/containers/MyGoals/MyGoals.js
+++ src/containers/MyGoals/MyGoals.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
-import {Motion, spring} from 'react-motion';
+//import {Motion, spring} from 'react-motion';
 //import {Redirect} from 'react-router-dom';
 
 import GoalCard from '../../components/GoalCard/GoalCard';
+import Animation from '../../hoc/Animation/Animation';
 import * as actions from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
-import classes from './MyGoals.css'
+//import classes from './MyGoals.css'
 
 
 class MyGoals extends Component {
@@ -47,47 +48,28 @@ class MyGoals extends Component {
             };
             listOfGoalsArrey.push(listElement);
         }
-        // Do not forget add spinner for loading later
-        
+
         let list = <Spinner />;
         if (!this.props.loading && this.props.goalsList === null) {
             list = <h2>Here you will see list of your goals when login/singup</h2>;
         } else if (!this.props.loading && this.props.goalsList !== null){
             list = (
-                <div>
+                <Animation editMode={this.props.editMode}>
                     {listOfGoalsArrey.map(element => {
                         return (
-                            <Motion style={{x: spring(this.props.editMode ? 200 : 100)}}  key={element.id} >
-                                { ({x}) => {
-                                    console.log('motion fiered')
-                                    return (
-                                    <div className={classes.AnimationConteniner}> 
-                                        <div className={classes.AnimatedComponent} style={{
-                                            WebkitTransform: `translate3d(${x}px, 0, 0)`,
-                                            transform: `translate3d(${x}px, 0, 0)`,                                                
-                                        }}
-                                        >
-                                            <GoalCard 
-                                                title={element.value.title}
-                                                clicked={!this.props.editMode ? () => this.combinedHandler(element.id, this.props.selectedGoalId): null}
-                                                delete={() => this.props.onDeleteGoal(this.props.token, this.props.userId, element.id)}
-                                                mode={this.props.editMode}
-                                            />
-                                        </div>
-                                    </div>);
-                                   }
-                                }
-                            </Motion>
+                            <GoalCard 
+                                key={element.id}
+                                title={element.value.title}
+                                clicked={!this.props.editMode ? () => this.combinedHandler(element.id, this.props.selectedGoalId): null}
+                                delete={() => this.props.onDeleteGoal(this.props.token, this.props.userId, element.id)}
+                                mode={this.props.editMode}
+                            />
                         );
                     })}
-                </div>
+                 </Animation>
             );
         }
-        return (
-            <div>
-               {list}
-            </div>
-        );
+        return list;
     }
 }
 
---
+++ src/hoc/Animation/Animation.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Motion, spring} from 'react-motion';
+
+import classes from './Animation.css'
+
+const animation = (props) => {
+    return (
+        <Motion style={{x: spring(props.editMode ? 200 : 100)}}>
+            { ({x}) => {
+                return (
+                    <div className={classes.AnimationConteniner}> 
+                        <div className={classes.AnimatedComponent} 
+                            style={{
+                                WebkitTransform: `translate3d(${x}px, 0, 0)`,
+                                transform: `translate3d(${x}px, 0, 0)`
+                            }}>
+                            {props.children}
+                        </div>
+                    </div>);
+                }
+            }
+        </Motion>);
+    }
+
+export default animation;
