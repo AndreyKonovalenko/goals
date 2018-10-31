264e4289865d45723510fec43b27bcb0d2590aaa
AndreyKonovalenko <andreinew@mail.ru>
2018-10-31 23:25:08 +0300

    Start implamentntig new style to SatusBoard colored boxes

--- src/components/StatusBoard/StatusBoard.js
+++ src/components/StatusBoard/StatusBoard.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-import cssObject from './StatusBoard.css';
+import classes from './StatusBoard.css';
 
 const statusBoard = (props) => {
     return (
-        <div className={cssObject.StatusBoard}>
+        <div className={classes.StatusBoard}>
             <h2>{props.title}</h2>
             <h4>started on {props.start}</h4>
             <p>all period is: {props.limitation} days</p>
             <p>days left: {props.left} </p>
+            <div className={classes.Succeeded}></div>
             <p>dyas succeeded: {props.succeeded}</p>
             <p>dyas failed:{props.failed}  </p>
         </div>
